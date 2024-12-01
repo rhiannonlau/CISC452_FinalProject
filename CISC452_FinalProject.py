22ac7e0f9cba956abb22ffc116dc9fcebb768056
rhiannonlau <rhiannoncglau@gmail.com>
2024-12-01 12:06:47 -0500

    preprocessing and data initialization complete

--- CISC452_FinalProject.py
+++ CISC452_FinalProject.py
@@ -14,36 +14,98 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 
 # DATA INITIALIZATION ###############################################
-# this one has some hindi in it??
-# twtData = np.genfromtxt('data/Twitter_Data.csv', delimiter=',')
-# twtData.shape
-
+# movie dataset
 mov_data = np.genfromtxt('data/movie.csv', delimiter=',')
 mov_data.shape
-# skip the first row
 mov_X = mov_data[1:, 0] # all rows except the first one, text column
 mov_y = mov_data[1:, 1] # all rows except the first one, label column
 
-# this one's data seems incomplete - lots of 0's
-# amzData = np.genfromtxt('data/amazon_reviews.csv', delimiter=',')
-# amzData.shape
+# chat gpt dataset
+cgpt_data = np.genfromtxt('data/chatgpt_sentiment_analysis.csv', delimiter=',')
+cgpt_data.shape
+cgpt_X = cgpt_data[1:, 1] # all rows except the first one, text column
+cgpt_y = cgpt_data[1:, 2] # all rows except the first one, label column
 
+# social media dataset
+sm_data = np.genfromtxt('data/soc_med_sentiment_analysis.csv', delimiter=',')
+sm_data.shape
+sm_X = sm_data[1:, 4] # all rows except the first one, text column
+sm_y = sm_data[1:, 5] # all rows except the first one, label column
+
+keys = ["mov", "cgpt", "sm"]
+
+X = {
+    "mov": mov_X,
+    "cgpt": cgpt_X,
+    "sm": sm_X
+}
+
+y = {
+    "mov": mov_y,
+    "cgpt": cgpt_y,
+    "sm": sm_y
+}
+
+X_train = { }
+
+X_test = { }
+
+y_train = { }
+
+y_test = { }
 
 # PRE-PROCESSING ###################################################
-# Normalize the data
-scaler = StandardScaler()
-mov_X = scaler.fit_transform(mov_X)
+# convert the labels of the cgpt and sm data to numeric labels
+# should i do a check to see make sure there's nothing outside of these labels?????????????
+labels = np.where(y["cgpt"] == "good", 1, np.where(y["cgpt"] == "neutral", 0, np.where(y["cgpt"] == "bad", -1, y)))
+y.update({"cgpt": labels})
 
-# Shuffle the data - necessary?
-# ind = []
-# for i in range(len(mov_X)):
-#     ind.append(i)
-# np.random.shuffle(ind)
-# mov_X = mov_X[ind]
-# mov_y = mov_y[ind]
+labels = np.where(y["sm"] == "positive", 1, np.where(y["sm"] == "neutral", 0, np.where(y["sm"] == "negative", -1, y)))
+y.update({"sm": labels})
 
-# Split data: training 80%, testing 20%
-split = int(0.8 * mov_X.shape[0])
-X_train, X_test = mov_X[:split], mov_X[split:]
-y_train, y_test = mov_y[:split], mov_y[split:]
+def preprocess(X, y):
+    # Normalize the data
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+
+    # Shuffle the data
+    indices = []
+
+    for i in range(len(X)):
+        indices.append(i)
+
+    np.random.shuffle(indices)
+    X = X[indices]
+    y = y[indices]
+
+    # Split data: training 80%, testing 20%
+    split = int(0.8 * X.shape[0])
+    X_train, X_test = X[:split], X[split:]
+    y_train, y_test = y[:split], y[split:]
+
+    # return X, X_train, X_test, y, y_train, y_test
+    return X_train, X_test, y_train, y_test
+
+for i in range(3):
+    key = keys[i]
+
+    # a, a_tr, a_ts, b, b_tr, b_ts = preprocess(X[key], y[key])
+    A_tr, A_ts, b_tr, b_ts = preprocess(X[key], y[key])
+
+    # X.update({key: A}) # does the X and y even matter anymore??
+    X_train.update({key: A_tr})
+    X_test.update({key: A_ts})
+    # y.update({key: b})
+    y_train.update({key: b_tr})
+    y_test.update({key: b_ts})
+
+# cnn
+def model():
+    pass
+
+def predict():
+    pass
+
+def accuracy():
+    pass
 
